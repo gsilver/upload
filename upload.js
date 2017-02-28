af932bdf95adfce988f75740f1911d5397889a1c
Silverio <gsilver@umich.edu>
2017-02-28 16:26:32 -0500

    add a select for function select

--- upload.js
+++ upload.js
@@ -41,13 +41,36 @@ myApp.service('fileUpload', ['$http', function($http) {
 }]);
 
 myApp.controller('myCtrl', ['$scope', 'fileUpload', '$timeout', function($scope, fileUpload, $timeout) {
-$scope.sortType='Id';
-$scope.content = false;
+  $scope.functions = [{
+    name: 'Add multiple users to Canvas sections through CSV',
+    id: 'users_in_sections'
+  }, {
+    name: 'Add multiple new sections to Canvas sections through CSV',
+    id: 'sections_to_canvas'
+  }, {
+    name: 'Add multiple new groups to Canvas sections through CSV',
+    id: 'groups_to_sections'
+  }, {
+    name: 'Add multiple users to Canvas groups through CSV',
+    id: 'users_to_groups'
+  }, {
+    name: 'Add multiple users to Canvas sections through data grid',
+    id: 'users_to_sections_grid'
+  }, {
+    name: 'Add multiple users to Canvas groups through data grid',
+    id: 'users_to-groups_grid'
+  }, {
+    name: 'Add course rosters to multiple Canvas sites',
+    id: 'rosters'
+  }];
+  //$scope.selectedFunction = $scope.functions[0];
+  $scope.sortType = 'Id';
+  $scope.content = false;
 
 
-$scope.resetFileUpload = function(){
-  $scope.content=[];
-};
+  $scope.resetFileUpload = function() {
+    $scope.content = [];
+  };
 
   $scope.$watch('users_in_sections', function(newFileObj) {
     if (newFileObj) {
@@ -85,28 +108,34 @@ $scope.resetFileUpload = function(){
   };
 
   parseCSV = function(CSVdata, headers, colCount) {
-      var lines=CSVdata.split("\n");
-      var result = [];
-      $scope.errors = [];
-      for(var i=0;i<lines.length;i++){
-        var lineArray =lines[i].split(',');
-        var obj ={'rowId':i};
-        for (var x=0;x<lineArray.length;x++){
-          if(lineArray[x] ==='' || lineArray[x].split(' ').length !==1){
-            obj.invalid = true;
-          }
-          obj[headers[x]] = lineArray[x];
-        }
-        if(lineArray.length !== colCount){
+    var lines = CSVdata.split("\n");
+    var result = [];
+    $scope.errors = [];
+    for (var i = 0; i < lines.length; i++) {
+      var lineArray = lines[i].split(',');
+      var obj = {
+        'rowId': i
+      };
+      for (var x = 0; x < lineArray.length; x++) {
+        if (lineArray[x] === '' || lineArray[x].split(' ').length !== 1) {
           obj.invalid = true;
         }
-        result.push(obj);
+        obj[headers[x]] = lineArray[x];
       }
-      if (_.where(result, {invalid: true}).length) {
-        $scope.errors = _.where(result, {invalid: true});
+      if (lineArray.length !== colCount) {
+        obj.invalid = true;
       }
-      $scope.loading = false;
-      return result;
+      result.push(obj);
+    }
+    if (_.where(result, {
+        invalid: true
+      }).length) {
+      $scope.errors = _.where(result, {
+        invalid: true
+      });
+    }
+    $scope.loading = false;
+    return result;
   };
 
 }]);
