312b835679a1949e97ae209462ec1e0fbac54317
Silverio <gsilver@umich.edu>
2017-03-02 15:32:26 -0500

    add structure to SIS upload file reader and grid request

--- upload.js
+++ upload.js
@@ -12,9 +12,6 @@ myApp.directive('fileModel', ['$parse', function($parse) {
           modelSetter(scope, element[0].files[0]);
         });
       });
-
-
-
     }
   };
 }]);
@@ -40,58 +37,178 @@ myApp.service('fileUpload', ['$http', function($http) {
   };
 }]);
 
-myApp.controller('myCtrl', ['$scope', 'fileUpload', '$timeout', function($scope, fileUpload, $timeout) {
+myApp.controller('myCtrl', ['$scope', '$rootScope','fileUpload', '$timeout', function($scope, $rootScope, fileUpload, $timeout) {
+  $scope.fields = [{
+    'name': 'user_id',
+    'validation': {
+      'spaces': 'nospaces',
+      'chars': 'num',
+      'max': 10,
+      'min': 10
+    }
+  }, {
+    'name': 'role',
+    'validation': {
+      'spaces': 'nospaces',
+      'chars': 'alpha',
+      'choices': ['choice1', 'choice2'],
+      'max': 10,
+      'min': 10
+    }
+  }, {
+    'name': 'section_id',
+    'validation': {
+      'spaces': 'nospaces',
+      'chars': 'num',
+      'max': 10,
+      'min': 10
+    }
+  }, {
+    'name': 'user_status',
+    'validation': {
+      'spaces': 'nospaces',
+      'chars': 'alpha',
+      'choices': ['choice1', 'choice2'],
+      'max': 10,
+      'min': 10
+    }
+  }, {
+    'name': 'section_name',
+    'validation': {
+      'spaces': 'spaces',
+      'chars': 'alpha',
+      'max': 10,
+      'min': 10
+    }
+  }, {
+    'name': 'group_name',
+    'validation': {
+      'spaces': 'spaces',
+      'chars': 'alpha',
+      'max': 10,
+      'min': 10
+    }
+  }, {
+    'name': 'section_id',
+    'validation': {
+      'spaces': 'nospaces',
+      'chars': 'num',
+      'max': 10,
+      'min': 10
+    },
+  }, {
+    'name': 'group_id',
+    'validation': {
+      'spaces': 'nospaces',
+      'chars': 'num',
+      'max': 10,
+      'min': 10
+    }
+  }, {
+    'name': 'section_status',
+    'validation': {
+      'spaces': 'nospaces',
+      'chars': 'alpha',
+      'choices': ['choice1', 'choice2'],
+      'max': 10,
+      'min': 10
+    }
+  }, {
+    'name': 'group_status',
+    'validation': {
+      'spaces': 'nospaces',
+      'chars': 'alpha',
+      'choices': ['accepted', 'deleted'],
+      'max': 10,
+      'min': 10
+    }
+  }, {
+    'name': 'user_uniqname',
+    'validation': {
+      'spaces': 'nospaces',
+      'chars': 'alpha',
+      'max': 10,
+      'min': 10
+    }
+  }, {
+    'name': 'user_role',
+    'validation': {
+      'spaces': 'nospaces',
+      'chars': 'alpha',
+      'choices': ['role1', 'role2'],
+      'max': 10,
+      'min': 10
+    }
+  }, ];
+
+  $scope.changeSelectedFunction = function(value){
+  $rootScope.selectedFunction = value;
+
+  };
   $scope.functions = [{
     name: 'Add multiple users to Canvas sections through CSV',
-    id: 'users_in_sections'
+    id: 'users_in_sections',
+    type: 'csv',
+    fields: ['user_id', 'role', 'section_id', 'user_status'],
+    fields_model : ['User ID', 'Role', 'Section Id', 'User Status']
+
   }, {
     name: 'Add multiple new sections to Canvas sections through CSV',
-    id: 'sections_to_canvas'
+    id: 'sections_to_canvas',
+    type: 'csv',
+    fields: ['section_name'],
+    fields_model: ['Section Name']
   }, {
     name: 'Add multiple new groups to Canvas sections through CSV',
-    id: 'groups_to_sections'
+    id: 'groups_to_sections',
+    type: 'csv',
+    fields: ['group_name'],
+    fields_model: ['Group Name']
   }, {
     name: 'Add multiple users to Canvas groups through CSV',
-    id: 'users_to_groups'
+    id: 'users_to_groups',
+    type: 'csv',
+    fields: ['user_id', 'group_id', 'status'],
+    fields_model: ['User Id', 'Group Id', 'Status']
   }, {
     name: 'Add multiple users to Canvas sections through data grid',
-    id: 'users_to_sections_grid'
+    id: 'users_to_sections_grid',
+    type: 'grid',
+    fields: ['section_name', 'user_uniqname', 'user_role'],
   }, {
     name: 'Add multiple users to Canvas groups through data grid',
-    id: 'users_to-groups_grid'
+    id: 'users_to-groups_grid',
+    type: 'grid',
+    fields: ['group_name', 'user_uniqname']
   }, {
     name: 'Add course rosters to multiple Canvas sites',
-    id: 'rosters'
+    id: 'rosters',
+    type: 'csv',
+    fields: ['notsure', 'dontknow', 'whoknows']
   }];
-  //$scope.selectedFunction = $scope.functions[0];
-  $scope.sortType = 'Id';
+
   $scope.content = false;
-
-
-  $scope.resetFileUpload = function() {
-    $scope.content = [];
+  $scope.gridRowNumber = 25;
+  $scope.getNumber = function(num) {
+      return new Array(num);
   };
 
-  $scope.$watch('users_in_sections', function(newFileObj) {
+  $scope.$watch('csvfile', function(newFileObj) {
     if (newFileObj) {
       $scope.content = false;
       $scope.loading = true;
       var reader = new FileReader();
       reader.readAsText(newFileObj);
-
-
       reader.onload = function(e) {
         var CSVPreview = function() {
-          $scope.headers = ['Id', 'Role', 'Section', 'Status'];
-          $scope.content = parseCSV(reader.result, $scope.headers, 4);
+          $scope.headers = $scope.selectedFunction.fields;
+          $scope.content = parseCSV(reader.result, $scope.headers, $scope.headers.length);
         };
         $timeout(CSVPreview, 100);
       };
-
       $scope.filename = newFileObj.name;
     }
   });
-
 
 
   $scope.uploadForm = function() {
@@ -113,15 +230,16 @@ myApp.controller('myCtrl', ['$scope', 'fileUpload', '$timeout', function($scope,
     $scope.errors = [];
     for (var i = 0; i < lines.length; i++) {
       var lineArray = lines[i].split(',');
-      var obj = {
-        'rowId': i
-      };
-      for (var x = 0; x < lineArray.length; x++) {
-        if (lineArray[x] === '' || lineArray[x].split(' ').length !== 1) {
-          obj.invalid = true;
+      var obj = {};
+      obj.data=[];
+      _.each(headers, function(cel, index){
+        if(lineArray[index]){
+          if(lineArray[index].split(' ').length !== 1 || lineArray[index] ===''){
+            obj.invalid = true;
+          }
         }
-        obj[headers[x]] = lineArray[x];
-      }
+        obj.data.push(lineArray[index]);
+      });
       if (lineArray.length !== colCount) {
         obj.invalid = true;
       }
